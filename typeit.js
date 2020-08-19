5f5cff1e9c07afc06252f193790ead8e852554fc
shreyas-shriyan <shreyshriyan@gmail.com>
2020-08-19 18:52:35 +0530

    updated tect fetching api

--- typeit.js
+++ typeit.js
@@ -1,5 +1,5 @@
 
-const RANDOM_TEXT_URL = 'http://quotes.stormconsultancy.co.uk/random.json' // Api for Fetching Data
+const RANDOM_TEXT_URL = 'https://api.quotable.io/random?minLength=70' // Api for Fetching Data
 
 let query = new URLSearchParams(window.location.search)
 let time = query.get('time')
@@ -49,7 +49,7 @@ user_text_element.addEventListener("input", ()=> {
 function get_random_text() {
     return fetch(RANDOM_TEXT_URL)
     .then((res)=>res.json())
-    .then(data => data.quote)
+    .then(data => data.content)
     /* .then(function(data){
         console.log(typeof(data.quote))
     }) */
